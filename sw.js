1470eacd0bc42308c34d93d7f274b0ee4c1a8583
nathanielwerner13-eng <nathanielwerner13@gmail.com>
2026-05-15 19:08:11 -0700

    sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('push', function(event) {
+    const data = event.data ? event.data.json() : {};
+    const title = data.title || 'Bina';
+    const options = {
+        body: data.body || '',
+        icon: '/icon-192.png',
+        badge: '/icon-192.png',
+        data: { url: data.url || '/' }
+    };
+    event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(clients.openWindow(event.notification.data.url));
+});
